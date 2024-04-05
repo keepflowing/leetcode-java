9b0dcf04c9d177f6fbff46f3ea9baf40ed38c81e
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-04-05 13:40:40 -0500

    Add lc no. 1

--- src/n1_twoSum/Testcases.java
+++ src/n1_twoSum/Testcases.java
@@ -1,7 +1,14 @@
 package n1_twoSum;
 
+import java.util.Arrays;
+
 public class Testcases {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int[][] numbers = {{2,7,11,15}, {3,2,4}, {3,3}};
+        int[] targets = {9, 6, 6};
+        for(int i = 0; i < numbers.length; i++) {
+            int[] res = TwoSum.findIndices(numbers[i], targets[i]);
+            System.out.println(Arrays.toString(res));
+        }
     }
 }
---
+++ src/n1_twoSum/TwoSum.java
@@ -0,0 +1,15 @@
+package n1_twoSum;
+
+import java.util.Hashtable;
+
+public final class TwoSum {
+    public static int[] findIndices(int[] nums, int target) {
+        Hashtable<Integer, Integer> hm = new Hashtable<>();
+        for(int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if(hm.containsKey(diff)) return new int[]{hm.get(diff), i};
+            hm.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
