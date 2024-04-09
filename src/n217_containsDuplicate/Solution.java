18b79caec91ea00d47f610976437932eb1162d9a
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-04-09 15:56:16 -0500

    Add lc no. 217

---
+++ src/n217_containsDuplicate/Solution.java
@@ -0,0 +1,15 @@
+package n217_containsDuplicate;
+
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums) {
+            if (set.contains(n)) return true;
+            set.add(n);
+        }
+        return false;
+    }
+}
