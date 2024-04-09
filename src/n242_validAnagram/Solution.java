b8cc7741fb65ec41609e3bee243cef748faa475f
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-04-09 16:17:07 -0500

    Add lc no. 242 (sorting solution)

---
+++ src/n242_validAnagram/Solution.java
@@ -0,0 +1,19 @@
+package n242_validAnagram;
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        char[] x = s.toCharArray();
+        char[] y = t.toCharArray();
+        Arrays.sort(x);
+        Arrays.sort(y);
+
+        String xS = new String(x);
+        String yS = new String(y);
+
+        return xS.equals(yS);
+    }
+}
