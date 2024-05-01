046346acb49248ff392b3df8b2471e0dcc7a53fe
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-05-01 11:21:39 -0500

    Uncomment ListNode class

--- src/n0021_mergetwosortedl/Solution.java
+++ src/n0021_mergetwosortedl/Solution.java
@@ -1,15 +1,13 @@
 package n0021_mergetwosortedl;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode();
